21128441bfd487b1cbc7320a7ad71722bb1db03b
William Parker <45488304+willparker123@users.noreply.github.com>
2022-08-14 21:54:59 +0100

    Large changelog; new README, deployment of sca-txscanner, instructions, CLI args

--- scripts/sniper.py
+++
@@ -1,9 +0,0 @@
-wallet_addresses = ["a", "b", "c"]
-private_keys = ["d", "e", "f"]
-private_wallet_ids: list[int] = [0, 1, 2]
-addressReceiver: str = "addr_rec"
-
-
-
-if __name__ == "__main__":
-    print("AA")
